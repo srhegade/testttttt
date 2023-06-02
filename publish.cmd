29fa11cd2a6ddf50a0f97240518f5a8778433c38
srhegade <46775072+srhegade@users.noreply.github.com>
2023-06-02 17:48:07 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\simplebotinstance-bot-d805-Web-Deploy.pubxml -p:Password=cZvGyshe4GZKe9dxvpZ6C7nK55sXJzgmmJloivH8xAxRf6yTadW1qfZrP4m4
+
